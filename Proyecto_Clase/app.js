23618ee7198dab6b13bb7b9d33289afe476fd289
ArielHL <73503229+ArielHL@users.noreply.github.com>
2022-10-18 22:24:55 -0300

    RENDER

--- Proyecto_Clase/app.js
+++ Proyecto_Clase/app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const path = require ('path');
 const app = express();
 
-const port = 3300;
+const port = process.env.PORT || 3001;
 
 // definir carpeta publica
 const publicPath = path.resolve('public')
